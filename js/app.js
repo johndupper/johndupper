63c3a30c416c6d75e83c5546b2d4c909f51528ad
John Dupper <john.dupper@me.com>
2016-10-24 18:26:35 -0400

    functionality is the same, ensured code passed jslint standards

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
+var $;
+
 $('#entire-page').fadeIn(1000);
 
 // reset page
-$('#navigation-name').click(function() {
+$('#navigation-name').click(function () {
+    'use strict';
     $('.body-content').hide();
     $('#image-main').fadeIn(500);
     $('#image-about').hide();
 });
 
 // toggle contact
-$('#nav-contact').click(function() {
+$('#nav-contact').click(function () {
+    'use strict';
     $('#contact-bar').slideToggle(150);
 });
 
 // about
-$('#nav-about').click(function() {
+$('#nav-about').click(function () {
+    'use strict';
     $('.body-content').hide();
     $('#about-me').fadeIn(500);
     $('#image-about').fadeIn(500);
@@ -25,32 +30,27 @@ var array = ['espresso', 'motorcycles', 'blue suede shoes', 'skinny jeans', 'Los
 var counter = 0;
 var elem = document.getElementById('rotate');
 
-setInterval(change, 1000);
 function change() {
+    'use strict';
     elem.innerHTML = array[counter];
-    counter++;
+    counter += 1;
     if (counter >= array.length) {
         counter = 0;
     }
 }
 
+setInterval(change, 1000);
+
 // code
-$('#nav-code').click(function() {
+$('#nav-code').click(function () {
+    'use strict';
     $('.body-content').hide();
     $('.code').fadeIn(500);
 });
 
-$('.post-title').click(function() {
-    $('.post-text').fadeOut(350);
-    $(this).siblings().fadeIn(350);
-});
-
-$('.back-to-top').click(function() {
-    $('.post-text').hide(350);
-});
-
 // education
-$('#nav-education').click(function() {
+$('#nav-education').click(function () {
+    'use strict';
     $('.body-content').hide();
     $('.education').fadeIn(350);
 });
